4ce08a66d1ef68b2594723690504f41ab35bda9b
AndradeLeroy <47416005+AndradeLeroy@users.noreply.github.com>
2019-12-15 06:21:03 +0100

    Javascript pre-structuur
    ^werkende draai knop ^generator ^teller toegevoegd

--- www/js/javaScript.js
+++ www/js/javaScript.js
@@ -1,1 +1,28 @@
-"" 
+//Generator
+let randomGenerator = Math.round(Math.random()*2);
+
+//Teller pogingen
+let iTeller = document.body.querySelector("#draaiTeller");
+
+//Fruit
+let ib = document.body.querySelector("#grid0");
+
+let fruitGenerator=["&#127813","&#127814;","&#127815;"]
+
+let teller = -0;
+let max= 100;
+let teksten = [];
+
+function Rekenen(){
+return randomGenerator = Math.floor(Math.random()*2);
+}
+
+function scramble()
+{
+teller+=1;
+
+
+iTeller.innerHTML=fruitGenerator[randomGenerator];
+Rekenen();
+ib.innerHTML=fruitGenerator[randomGenerator];
+}
